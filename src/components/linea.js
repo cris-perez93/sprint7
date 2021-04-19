2c9971a0e374fedba922d3b9ebbff175dd0aca3c
Cris <cristian.h.perez.o@gmail.com>
2021-04-19 19:03:23 +0100

    level2

--- src/components/linea.js
+++ src/components/linea.js
@@ -1,11 +1,29 @@
+import styled from '@emotion/styled';
 
+
+const Container = styled.div`
+max-width: 500px;
+background-color: rgba(68, 65, 65, 0.616);
+margin : 0 auto;
+
+`;
+
+const Frase = styled.p `
+  font-family: 'Montserrat', sans-serif;
+  color: whitesmoke;
+  border-radius:10px;
+  font-weight: bold;
+  padding:10px;
+  
+  
+`;
 
 
 const Linea = (props) => {
     return (
-        
-            <p>{props.texto}</p>
-            
+        <Container>
+            <Frase>{props.texto}</Frase>
+        </Container>
         
       );
 }
