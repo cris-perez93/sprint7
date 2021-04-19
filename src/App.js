c4c4eb9a37c2f0764ff24350d854d1273e0a0391
Cris <cristian.h.perez.o@gmail.com>
2021-04-19 18:45:28 +0100

    subida

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {Fragment} from 'react'
+import Linea from './components/linea'
+
+
+
+
 
 function App() {
+
+  
+const texto = [
+
+  "Hola Romeo.", 
+ 
+ "Hola Julieta", 
+ 
+ "T'estimo, Romeo", 
+ 
+ "I jo a tu Julieta", 
+ 
+ "Escapem-nos a Mèxic", 
+ 
+ "Millor no, no li caus bé a la meva família i si seguim endavant amb el nostre amor, aquesta història acabarà malament", 
+ 
+ "Doncs vaja" 
+ 
+ ] 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Fragment>
+      
+      {Array.from(texto).map(t => {
+        
+      return <Linea
+         
+         texto = {t}
+      />
+      })}
+     
+    </Fragment>
+    
+    
+  )
+};
 
 export default App;
---
+++ src/components/linea.js
@@ -0,0 +1,13 @@
+
+
+
+const Linea = (props) => {
+    return (
+        
+            <p>{props.texto}</p>
+            
+        
+      );
+}
+ 
+export default Linea;
